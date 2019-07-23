d3d2f2d76e67ea9087c9f7dc151be2e7c9c6bde4
rafachuichui <rmiguezr@gmail.com>
2019-07-23 17:58:16 +0200

    falta desplegar

--- routes/users.js
+++ routes/users.js
@@ -1,94 +1,92 @@
 const express = require('express');
-const users = express.Router();
-const cors = require('cors');
+const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const user = require("../model/user");
-users.use(cors());
+const mysql = require('../database/db')
+
 process.env.SECRET_KEY = 'secret'
 
 //REGISTER
-users.post('/register', (req, res) => {
-    const today = new Date();
+router.post('/register', async (req, res) => {
+    const today = new Date().toString();
     const userData = {
-        nombre: req.body.name,
-        password: req.body.password,
+        nombre: req.body.nombre,
+        password: bcrypt.hashSync(req.body.password, 10),
         email: req.body.email,
         telefono: req.body.telefono,
         direccion: req.body.direccion,
         created: today
+    };
+
+    const mysqlQuery = 'INSERT INTO users SET ?';
+    try {
+        const connection = await mysql.getConnection();
+
+        const user = await connection.query('SELECT * FROM users');
+
+        await connection.query(mysqlQuery, userData);
+
+        connection.release();
+    } catch (error) {
+        return res.send(error);
     }
-    user.findeOne({
-        where: {
-            email: req.body.email
-        }
-    })
 
-        .then(user => {
-            if (!user) {
-                const hash = bcrypt.hashSync(userData.password, 10)
-                userData.password = hash
-                user.create(userData)
-                    .then(user => {
-                        let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-                            expiresIn: 1440
-                        })
-                        res.json({ token: token })
-                    }).cathc(err => {
-                        res.send('error: ' + err)
-                    })
-            } else {
-                res.json({ error: 'User already exists' })
-            }
-        })
-        .cathc(err => {
-            res.send('error: ' + err)
-        });
-
-
-    //LOGIN
-    users.post('/login', (req, res) => {
-        user.findeOne({
-            where: {
-                email: req.body.email
-            }
-        })
-            .then(user => {
-                if (bcrypt.compareSync(req.body.password, user.password)) {
-                    let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-                        expiresIn: 1440
-                    })
-                    res.json({ token: token })
-                } else {
-                    res.send('user does not exist')
-                }
-            })
-            .cathc(err => {
-                res.send('error: ' + err)
-            })
-
-    });
-
-
-    //PROFILE
-    users.get('/profile', (req, res) => {
-        const decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
-
-        User.findeOne({
-            where: { userid: decoded.userid }
-        })
-            .then(user => {
-                if (user) {
-                    res.json(user)
-                } else {
-                    res.send('User does not exist')
-                }
-            })
-            .cathc(err => {
-                res.send('error: ' + err)
-            })
-    })
+    res.status(201).send();
 })
 
-module.exports = users
+
+//LOGIN
+router.post('/login', async (req, res) => {
+    const accountData = req.body;
+
+    try {
+        const mysqlQuery = `SELECT userid, email, password FROM users WHERE email='${accountData.email}'`;
+        const connection = await mysql.getConnection();
+        const [userDB] = await connection.query(mysqlQuery);
+        if (userDB.length !== 1) {
+            return res.status(401).send();
+        }
+
+        const userData = userDB[0];
+        if (bcrypt.compareSync(accountData.password, userData.password)) {
+            return res.status(401).send();
+        }
+        const tokenJwt = jwt.sign({
+            userid: userData.userid,
+            email: userData.email,
+        }, process.env.AUTH_JWT_SECRET,
+            { expiresIn: parseInt(process.env.AUTH_ACCESS_TOKEN_TTL, 10) })
+
+        return res.status(200).send({
+            accessToken: tokenJwt,
+            expiresIn: parseInt(process.env.AUTH_ACCESS_TOKEN_TTL, 10),
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+
+
+// //PROFILE
+// router.get('/profile', (req, res) => {
+//     const decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+//     User.findeOne({
+//         where: { userid: decoded.userid }
+//     })
+//         .then(user => {
+//             if (user) {
+//                 res.json(user)
+//             } else {
+//                 res.send('User does not exist')
+//             }
+//         })
+//         .cathc(err => {
+//             res.send('error: ' + err)
+//         })
+// })
+
+
+
+module.exports = router
